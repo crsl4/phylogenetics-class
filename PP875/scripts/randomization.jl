65b6826b9b1a0b3dd947f7e8fa00d59464cb8dd7
ClaudiaSL <solislemus@wisc.edu>
2021-02-26 09:55:26 -0600

    updated journal club teams

--- PP875/scripts/randomization.jl
+++ PP875/scripts/randomization.jl
@@ -1,12 +1,16 @@
 ## Julia script to randomize roles in jigsaw dynamic
 ## Claudia January 2021
 
-people = ["m.b", "t.c", "c.f", "l.f", "a.h", "l.h", "d.j", "m.k.","a.k", "m.s", "j.s.g", "j.s", "b.t", "k.d."]
-n = length(people)
-
 ## February 5, 12, 19, 26
-using Random
+people = ["m.b", "t.c", "c.f", "l.f", "a.h", "l.h", "d.j", "m.k.","a.k", "m.s", "j.s.g", "j.s", "b.t", "k.d."]
 s = 0130140
+
+## March 5, 12, 19, 26
+people = ["austin","ben","charlotte","dana", "grant", "jose", "kymi", "lauren","lillian", "max", "melody", "yurin"]
+s = 02262021954
+
+using Random
+n = length(people)
 Random.seed!(s);
 people = people[randperm(length(people))]
 group1 = people[1:Integer(n/2)]
